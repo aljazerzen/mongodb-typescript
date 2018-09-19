d4955bf55dd06ac728698f07e6c0f93fc50a6148
aljazerzen <aljaz.erzen@gmail.com>
2018-09-19 13:45:42 +0200

    build: add repo information

--- test/mongo.ts
+++ test/mongo.ts
@@ -1,14 +1,14 @@
-import mongodb, { MongoClient } from 'mongodb';
+import * as mongodb from 'mongodb';
 
 export async function connect() {
-  const url = process.env.MONGO_URL || 'mongodb://localhost:27017/mongoose-typescript';
+  const url = process.env.MONGO_URL || 'mongodb://localhost:27017/mongodb-typescript';
   return await mongodb.connect(url, { useNewUrlParser: true });
 }
 
-export async function clean(mongo: MongoClient) {
+export async function clean(mongo: mongodb.MongoClient) {
   await mongo.db().dropDatabase();
 }
 
-export async function close(mongo: MongoClient) {
+export async function close(mongo: mongodb.MongoClient) {
   await mongo.close();
 }
